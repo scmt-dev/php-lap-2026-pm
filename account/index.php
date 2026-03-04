3f14d00703b3d2c19f83b16c600a8cc51904f069
Mark AI <chanthavong_12@hotmail.com>
2026-03-04 18:52:12 +0700

    add account index page with initial table structure

---
+++ account/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+</head>
+<body>
+    
+<div>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Balance</th>
+        </tr>
+        <tr>
+            <td>10001010</td>
+            <td>Dev</td>
+            <td>0.00</td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
