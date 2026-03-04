8d013b4777ac54d0c4d16d67ceb63a22984ed6af
Mark AI <chanthavong_12@hotmail.com>
2026-03-04 20:06:01 +0700

    Add header and footer to create account page and implement currency selection

--- account/create.php
+++ account/create.php
@@ -1,16 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Account</title>
-</head>
-<body>
+<?php
+ include_once 'header.php';
+?>
     <form action="" method="post">
+        <h1>Create Account</h1>
         <div>
             Account Number
-            <input type="text" />
+            <input type="text" name="account_number" />
         </div>
+        <div>
+            Currency
+           <select name="currency" id="currency">
+                <option value="LAK">LAK</option>
+                <option value="THB">THB</option>
+                <option value="USD">USD</option>
+           </select>
+        </div>
+        <div>
+            User
+           <select name="user" id="user">
+                
+           </select>
+        </div>
+        <button type="submit" value="Create" name="submit">Create</button>
     </form>
-</body>
-</html>
+<?php
+    include_once 'footer.php';
+?>
